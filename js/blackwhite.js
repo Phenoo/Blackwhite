24a6969e41b48d5d866f1bdfd305c1122fe8676f
phenopreneur <54966250+phenopreneur@users.noreply.github.com>
2022-02-28 03:16:51 +0100

    this is good

---
+++ js/blackwhite.js
@@ -0,0 +1,40 @@
+// classList - shows/gets all classes
+// contains - checks classList for specific class
+// add - add class
+// remove - remove class
+// toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function() {
+    // console.log(links.classList);
+    // console.log(links.classList.contains("random"));
+    // console.log(links.classList.contains("links"));
+    // if (links.classList.contains("show-links")) {
+    //   links.classList.remove("show-links");
+    // } else {
+    //   links.classList.add("show-links");
+    // }
+    links.classList.toggle("show-links");
+    links.style.display = "block";
+});
+
+
+
+
+//---------SCROLL REVEAL//
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true  
+})
+
+
+
+sr.reveal(`.container1,.container2, .container4, .links`)
+sr.reveal(`.social-links, .container3, .container5, .foot, .marker, .logo`, { origin: "left" })
+sr.reveal(`.divider, .copytext , .scrollup`, { origin: "bottom" })
+sr.reveal(`.container6,.container7, .nav-btn`, { origin: 'right' })
